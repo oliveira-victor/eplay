7d06aa5e9ff81db48468a644e635a3c57a9676db
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-01-13 17:50:32 -0300

    componentizando - parte I

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom'
 
 import { cores } from '../../styles'
 
-export const ButtonContainer = styled.button`
-  border: 2px solid ${cores.branca};
+import { Props } from '.'
+
+export const ButtonContainer = styled.button<Props>`
+  border: 2px solid
+    ${(Props) => (Props.variant === 'primary' ? cores.verde : cores.branca)};
   color: ${cores.branca};
-  background-color: transparent;
+  background-color: ${(Props) =>
+    Props.variant === 'primary' ? cores.verde : 'transparent'};
   font-size: 16px;
   font-weight: bold;
   padding: 8px 16px;
@@ -20,4 +24,5 @@ export const ButtonLink = styled(Link)`
   font-weight: bold;
   padding: 8px 16px;
   text-decoration: none;
+  border-radius: 8px;
 `
