40534a6d5acc7acc0f9d3ce13d7d9d3a92c1bf79
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-01-22 11:47:51 -0300

    feat: configure redux api into pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import Header from './components/Header'
@@ -5,18 +6,21 @@ import { GlobalCss } from './styles'
 
 import Rotas from './routes'
 import Footer from './components/Footer'
+import { store } from './store'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <GlobalCss />
-        <div className="container">
-          <Header />
-        </div>
-        <Rotas />
-        <Footer />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <GlobalCss />
+          <div className="container">
+            <Header />
+          </div>
+          <Rotas />
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </div>
   )
 }
